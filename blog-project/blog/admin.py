5d091816eed6313639d847bbf49e86b659f2663d
AlvaroTV <alvaro.etv@gmail.com>
2024-10-01 07:48:24 -0600

    Add django app

---
+++ blog-project/blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from blog.models import Tag, Post
+
+# Register your models here.
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+    # We add the slug field automatically (depends on the title)
+    prepopulated_fields = {"slug": ("title",)}
+    # We use list_display to cahnge the information presented in the Post List, in this case we'll show the slug and the date it published
+    list_display = ("slug", "published_at")
+
+# Add the class PostAdmin, with this we add the properties in that class.
+admin.site.register(Post, PostAdmin)
